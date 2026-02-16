b9d77de0d61183e0dde289230dc94330d6da8b61
mohamed231141758 <ophamed223@gmail.com>
2026-02-16 07:15:32 -0800

    Add BookViewSet with CRUD operations and DefaultRouter config

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+router.register(r'books_all', views.BookViewSet, basename='book-all')
 
 urlpatterns = [
     path('books/', views.BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 ]
