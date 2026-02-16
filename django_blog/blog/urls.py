1e2d02020d80db9821d98a796992226780bd8f86
mohamed231141758 <ophamed223@gmail.com>
2026-02-16 11:23:22 -0800

    Fix URL patterns to match post/<int:pk>/delete/, update/, and post/new/

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
 
     # Post URLs
     path('', views.PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-edit'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-edit'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
